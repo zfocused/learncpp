51fc7bab5b02e93a8dee029e90dcbcd3cbafd5e0
zfocused <162071357+zfocused@users.noreply.github.com>
2026-07-14 09:51:01 +0800

    2nd commit

--- learncpp.cpp
+++ learncpp.cpp
@@ -18,6 +18,7 @@ int main()
 	std::cout << "-----------------------------------------" << std::endl;
 	*/
 
+	/*	
 	std:: vector<unsigned> scores(11, 0); // 11 buckets, all initially 0
 	unsigned grade;
 	while (std::cin >> grade) { // read the grades
@@ -27,7 +28,24 @@ int main()
 	std::cout <<" ---------printing scores-------------" <<std::endl;
 	for (int i = 0; i < 10; ++i) {
 		std::cout << scores[i] << " ";
-	}
 
-	std::cout <<std::endl;
-}
+	std::cout <<std::endl
+	*/
+
+	/*
+	std::string test_string = "This is a test for string iteration";
+	for (std::string::iterator sitr = test_string.begin(); sitr != test_string.end(); ++sitr)
+		std::cout << *sitr;
+	std::cout << std::endl;
+	*/
+	
+	std::string test_string = "This is another test for string iteration";
+	for (auto sitr2 = test_string.begin(); sitr2 != test_string.end(); ++sitr2)
+		std::cout << *sitr2;
+	std::cout << std::endl;
+	std::cout << "-----------------------------------------------" <<std::endl;
+
+	char *char_ptr = "Who are you?";
+
+	
+}	
